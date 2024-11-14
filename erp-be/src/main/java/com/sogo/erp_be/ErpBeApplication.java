9d6f0f9f3fd54eaae51e61c8dab2be84c0e3dbd1
kallagoutham <kallagoutham33@gmail.com>
2024-11-13 20:46:26 -0500

    Initialize BE and FE

---
+++ erp-be/src/main/java/com/sogo/erp_be/ErpBeApplication.java
@@ -0,0 +1,13 @@
+package com.sogo.erp_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpBeApplication.class, args);
+	}
+
+}
